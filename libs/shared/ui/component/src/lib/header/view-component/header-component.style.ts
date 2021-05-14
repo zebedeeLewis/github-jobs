33249e9c1ffc6d69f71cac64bd4890b5048f529e
Zebedee <zebedeeslewis@gmail.com>
2021-05-14 08:22:12 -0600

    Update Header Component Hight & Top Padding

--- libs/shared/ui/component/src/lib/header/view-component/header-component.style.ts
+++ libs/shared/ui/component/src/lib/header/view-component/header-component.style.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import * as theme from '@shared/ui/theme'
 
 const HEADER_HEIGHT_PX = 136
+const HEADER_TOP_PADDING_PX = 32
 
 export type Props = {
   themeScheme: theme.Color.ThemeScheme
@@ -10,6 +11,8 @@ export type Props = {
 
 export const StyledHeaderComponent = styled.div<Props>`
   height: ${HEADER_HEIGHT_PX}px;
+  padding-top: ${HEADER_TOP_PADDING_PX}px;
+  box-sizing: border-box;
 
   ${(props) => theme.Color.withBgPrimary(props.themeScheme)}
 `
