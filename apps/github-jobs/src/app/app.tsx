349d57acfb8002d3546a4dda689cbb1506209879
Zebedee <zebedeeslewis@gmail.com>
2021-05-13 18:26:22 -0600

    Update Header To Display Logo

--- apps/github-jobs/src/app/app.tsx
+++ apps/github-jobs/src/app/app.tsx
@@ -1,26 +1,26 @@
 import styled from 'styled-components'
 import {
   Route,
-  Link,
   Switch,
   BrowserRouter as Router,
 } from 'react-router-dom'
 
+import { Header } from '@shared/ui/component'
+
 import * as HomePage from './page/home'
 import * as DetailsPage from './page/details'
 import * as NotFoundPage from './page/404'
+import Logo from '../assets/image/logo.svg'
 
 const StyledApp = styled.div`
-  font-family: sans-serif;
-  min-width: 300px;
-  max-width: 600px;
-  margin: 50px auto;
+  color: black;
 `
 
 export function App() {
   return (
     <StyledApp>
       <Router>
+        <Header.View logoSrc={Logo} logoAlt="Logo" />
         <Switch>
           <Route exact path="/" component={HomePage.View} />
           <Route path="/details/:id" component={DetailsPage.View} />
