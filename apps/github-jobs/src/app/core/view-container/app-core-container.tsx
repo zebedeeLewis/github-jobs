980cfdcd9df6886ffc335cb5735c8cd37e45452c
Zebedee <zebedeeslewis@gmail.com>
2021-05-16 08:19:10 -0600

    Update AppContainer To Use Api Module
      Status: The AppContainer is using a dummy jobs array to temporarily
      serve as the data for the application.
    
      Problem: The needs to be able to handle getting the data from a remote
      api.
    
      Solution: Update the AppContainer to request the data using the Api
      module.

--- apps/github-jobs/src/app/core/view-container/app-core-container.tsx
+++ apps/github-jobs/src/app/core/view-container/app-core-container.tsx
@@ -1,14 +1,26 @@
 import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 
 import * as Theme from '@shared/ui/theme'
 import { AppComponent } from '../view-component'
 import * as State from '../state'
 import * as Action from '../action'
+import * as Api from '../../api'
 
 export const AppContainer = () => {
+  const dispatch = useDispatch()
   const themeScheme = useSelector(State.getThemeScheme)
   const jobs = useSelector(State.getJobs)
-  const dispatch = useDispatch()
+  const mustLoadNextPage = useSelector(State.getMustLoadNextPage)
+  const currentPage = useSelector(State.getCurrentPage)
+
+  useEffect(() => {
+    if (mustLoadNextPage) {
+      Api.getPage(currentPage + 1).then((jobs) =>
+        dispatch(Action.updateJobs(jobs))
+      )
+    }
+  })
 
   const props = {
     jobs,
@@ -17,8 +29,8 @@ export const AppContainer = () => {
     toggleThemeScheme: () => {
       dispatch(
         Theme.isLight(themeScheme)
-          ? Action.createToggleDarkModeOn()
-          : Action.createToggleDarkModeOff()
+          ? Action.toggleDarkModeOn()
+          : Action.toggleDarkModeOff()
       )
     },
   }
