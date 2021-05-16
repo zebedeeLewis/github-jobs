f16c630cb8501d0bb07b8471255f2a38c1b09cca
Zebedee <zebedeeslewis@gmail.com>
2021-05-16 08:28:11 -0600

    Add New App Core State Fields, Curry Filed Setters
      - Add "mustLoadNextPage" field to app core state.
    
        Status: A means to know when to load the next page has not been
        implimented.
    
        Problem: The application will not know when to load the next page.
    
        Solution: Add a "mustLoadNextPage" field to the app core state.
        Whenever a new page is to be loaded, this field should be set to
        true (the constant MUST_LOAD_NEXT_PAGE has been added to act as an
        aliase for true when used to set the value for the mustLoadNextPage
        field). Then the next time the application renders, it will request
        another page from the api.
    
      - Add "currentPage" field.
    
        Status: There is no way of knowing which page to load next.
    
        Problem: if there is no way to know which page to load next we will
        not be able to load pages in a sequence.
    
        Solution: add a "currentPage" field to the core app state. Whenever
        a page is loaded, this field will be set to the page number of the
        most recently loaded page.
    
      - Curry all app core state field setter is the.
    
        Status: The setters all take 2 arguments, an state model and
        the value to assign to the appropriate field. The function is
        uncurried.
    
        Problem: To compose the multiple setters we must first got through
        the extra step of uncurrying the function and then compose.
    
        Solution: Design the setter functions to be curried.

--- apps/github-jobs/src/app/core/state/app-core-state.ts
+++ apps/github-jobs/src/app/core/state/app-core-state.ts
@@ -1,8 +1,14 @@
 import * as Theme from '@shared/ui/theme'
 import * as Job from '../../job'
 
+export const MUST_LOAD_NEXT_PAGE = true
+export const MUST_NOT_LOAD_NEXT_PAGE = false
+export const NO_PAGE_LOADED = 0
+
 export interface Model {
   themeScheme: Theme.ThemeScheme
+  mustLoadNextPage: boolean
+  currentPage: number
   jobs: Array<Job.State.Model>
 }
 
@@ -16,10 +22,70 @@ export interface Model {
  */
 export const create = ({
   themeScheme = Theme.LIGHT,
+  mustLoadNextPage = MUST_LOAD_NEXT_PAGE,
+  currentPage = NO_PAGE_LOADED,
   jobs = [],
 }: Partial<Model>): Model => ({
   themeScheme,
-  jobs: Job.State.sampleJobs,
+  mustLoadNextPage,
+  currentPage,
+  jobs,
+})
+
+/**
+ * Get the currentPage from the given state model.
+ *
+ * @param model - the state model from which we want to extract the
+ * currentPage.
+ *
+ * @returns The currentPage from the given state model.
+ */
+export const getCurrentPage = (model: Model): number =>
+  model.currentPage
+
+/**
+ * Set the currentPage value of the given state model.
+ *
+ * @param currentPage - the new value.
+ *
+ * @param model - the state model on which we want to set the
+ * currentPage value.
+ *
+ * @returns The currentPage from the given state model.
+ */
+export type SetCurrentPage = (v: number) => (s: Model) => Model
+
+export const setCurrentPage: SetCurrentPage = currentPage => model => ({
+  ...model,
+  currentPage,
+})
+
+/**
+ * Get the mustLoadNextPage from the given state model.
+ *
+ * @param model - the state model from which we want to extract the
+ * mustLoadNextPage.
+ *
+ * @returns The mustLoadNextPage from the given state model.
+ */
+export const getMustLoadNextPage = (model: Model): boolean =>
+  model.mustLoadNextPage
+
+/**
+ * Set the mustLoadNextPage value of the given state model.
+ *
+ * @param mustLoadNextPage - the new value.
+ *
+ * @param model - the state model on which we want to set the
+ * mustLoadNextPage value.
+ *
+ * @returns The mustLoadNextPage from the given state model.
+ */
+export type SetMustLoadNextPage = (v: boolean) => (s: Model) => Model
+
+export const setMustLoadNextPage: SetMustLoadNextPage = mustLoadNextPage => model => ({
+  ...model,
+  mustLoadNextPage,
 })
 
 /**
@@ -36,17 +102,18 @@ export const getThemeScheme = (model: Model): Theme.ThemeScheme =>
 /**
  * Set the theme scheme of the given state model.
  *
+ * @param themeScheme - the new value of the theme scheme.
+ *
  * @param model - the state model on which we want to set the
  * theme scheme.
  *
- * @param themeScheme - the new value of the theme scheme.
- *
  * @returns The themeScheme from the given state model.
  */
-export const setThemeScheme = (
-  model: Model,
-  themeScheme: Theme.ThemeScheme
-): Model => ({
+export type SetThemeScheme = (
+  v: Theme.ThemeScheme
+) => (s: Model) => Model
+
+export const setThemeScheme: SetThemeScheme = themeScheme => model => ({
   ...model,
   themeScheme,
 })
@@ -65,17 +132,16 @@ export const getJobs = (model: Model): Array<Job.State.Model> =>
 /**
  * Set the jobs array of the given state model.
  *
+ * @param jobs - the new value of the jobs array.
+ *
  * @param model - the state model on which we want to set the jobs
  * array.
  *
- * @param jobs - the new value of the jobs array.
- *
  * @returns The jobs array from the given state model.
  */
-export const setJobs = (
-  model: Model,
-  jobs: Array<Job.State.Model>
-): Model => ({
+export type SetJobs = (v: Array<Job.State.Model>) => (s: Model) => Model
+
+export const setJobs: SetJobs = jobs => model => ({
   ...model,
   jobs,
 })
--- apps/github-jobs/src/app/core/state/index.ts
+++ apps/github-jobs/src/app/core/state/index.ts
@@ -1,11 +1,29 @@
 import {
+  MUST_LOAD_NEXT_PAGE,
+  MUST_NOT_LOAD_NEXT_PAGE,
   create,
   getThemeScheme,
   setThemeScheme,
+  getMustLoadNextPage,
+  setMustLoadNextPage,
+  getCurrentPage,
+  setCurrentPage,
   getJobs,
   setJobs,
 } from './app-core-state'
 import type { Model } from './app-core-state'
 
-export { create, getThemeScheme, setThemeScheme, getJobs, setJobs }
+export {
+  MUST_LOAD_NEXT_PAGE,
+  MUST_NOT_LOAD_NEXT_PAGE,
+  create,
+  getThemeScheme,
+  setThemeScheme,
+  getMustLoadNextPage,
+  setMustLoadNextPage,
+  getCurrentPage,
+  setCurrentPage,
+  getJobs,
+  setJobs,
+}
 export type { Model }
